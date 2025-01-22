2f2264d6b28ecc7995285b07291d5c6547c15a26
BalintXHD <36235183+BalintXHD@users.noreply.github.com>
2025-01-22 13:05:02 +0100

    asd

--- scenes/deepseek/test_player.gd
+++ scenes/deepseek/test_player.gd
@@ -1,9 +1,10 @@
 extends KinematicBody2D
 
 var velocity := Vector2()
-var speed = 500
+var speed = 375
 var x = (180/PI)
 var last = 0
+var last2 = 0
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -20,6 +21,9 @@ func _physics_process(delta):
 
 func _on_VirtualJoystick2_analogic_chage(move):
 	get_node("Gun").rotation_degrees = move.angle()*x
+	if(abs(move.angle()*x) == 0):
+		get_node("Gun").rotation_degrees = last2
+	last2 = move.angle()*x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -31,10 +35,13 @@ func _on_VirtualJoystick_analogic_chage(move):
 	print(move.angle()*x)
 	if(abs(move.angle()*x) >= 90):
 		get_node("CollisionShape2D/Sprite").flip_h = true
+		get_node("CollisionShape2D/Sprite").play("default",true)
 	else:
+		get_node("CollisionShape2D/Sprite").play("default",false)
 		get_node("CollisionShape2D/Sprite").flip_h = false
 
 	if(velocity == Vector2(0,0)):
+		get_node("CollisionShape2D/Sprite").stop()
 		if(abs(last) >= 90):
 			get_node("CollisionShape2D/Sprite").flip_h = true
 		else:
